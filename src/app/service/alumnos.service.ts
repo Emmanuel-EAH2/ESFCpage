1bec701e526817edcb7ce49eab353335fd2a4aa8
Emmanuel-EAH2 <cetis634@gmail.com>
2021-01-02 20:05:33 -0600

    agregue las carpeta de alumnado y coordinacion

---
+++ src/app/service/alumnos.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { alumnosModel } from "../models/alumnos";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlumnosService {
+
+  constructor(private http: HttpClient) { }
+
+  getAlumnos(){
+    return this.http.get('http://localhost:3003/alumnos').toPromise();
+ }
+
+  postAlumnos(alumno: alumnosModel){
+    return this.http.post('http://localhost:3003/alumnos', alumno).toPromise();
+ }
+
+
+}
